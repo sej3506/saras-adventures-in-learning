c7cd77486b141fff7dd9a7ac9ce3869d2543cb9d
Sara Jackson <csara@thoughtbot.com>
2024-09-24 15:36:53 -0400

    Update solargraph script - formatting and continue in face of some errors

--- bin/solargraph_setup.sh
+++ bin/solargraph_setup.sh
@@ -3,8 +3,8 @@
 # Exit if any subcommand fails
 set -e
 
-echo '\n=========== adding VSCode cli to PATH ===========\n'
-if command -v code; then
+printf "\n=================== adding VSCode cli to PATH ===================\n"
+if command -v code > /dev/null; then
   printf "VSCode cli is already installed!\n"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   printf "VSCode cli is NOT installed. Starting setup!\n"
@@ -18,37 +18,38 @@ EOF
 else
   printf "Currently only supporting adding CLI to PATH for macOS.\n"
   printf "Check out instructions for your OS here https://code.visualstudio.com/docs/setup/setup-overview\n"
+  exit 1
 fi
 
-echo '\n=== installing solargraph extension ===\n'
+printf "\n============== installing solargraph extension ================\n"
 code --install-extension castwide.solargraph
 
-echo '\n================ installing solargraph gem ================\n'
+printf "\n================= installing solargraph gem ===================\n"
 gem install solargraph
 
 if [ ! -f .solargraph.yml ]; then
-  echo '\n================ adding .solargraph.yml ================\n'
+  printf "\n==================== adding .solargraph.yml =====================\n"
   touch .solargraph.yml
   echo "include:" > .solargraph.yml
   echo "- \"app/**/*.rb\"" >> .solargraph.yml
   echo "- \"spec/**/*.rb\"" >> .solargraph.yml
 fi
 
-echo '\n================ installing gem docs ================\n'
-yard gems
+printf "\n====================== installing gem docs ======================\n"
+yard gems || true
 
-echo '\n================ setting gem docs to auto-install ================\n'
+printf "\n================ setting gem docs to auto-install ===============\n"
 yard config --gem-install-yri
 
 if [ ! -f config/definitions.rb ]; then
-  echo '\n================ adding config/definitions.rb ================\n'
+  printf "\n================= adding config/definitions.rb ==================\n"
   touch config/definitions.rb
   url='https://gist.githubusercontent.com/castwide/28b349566a223dfb439a337aea29713e/raw/715473535f11cf3eeb9216d64d01feac2ea37ac0/rails.rb'
   html=$( curl -# -L "${url}" 2> '/dev/null' )
   echo "${html}" > config/definitions.rb
 fi
 
-echo '\n================ installing NPM ================\n'
+printf "\n========================= installing NPM ========================\n"
 NPM_INSTALLED=$(command asdf plugin list all | grep -c npm)
 if [ $NPM_INSTALLED -gt 0 ]; then
   printf "npm plugin for asdf is already added!\n"
@@ -57,8 +58,8 @@ else
   asdf plugin add npm
 fi
 
-echo '\n================ installing NPM json ================\n'
-if command -v json; then
+printf "\n====================== installing NPM json ======================\n"
+if command -v json > /dev/null; then
   printf "npm json is already installed!\n"
 else
   printf "npm json is NOT installed. Starting setup!\n"
@@ -78,12 +79,12 @@ fi
 
 solargraphpath=$(command -v solargraph)
 
-echo '\n================ updating Solargraph settings ================\n'
+printf "\n================= updating Solargraph settings ==================\n"
 json -I -f "${settings_path}" -e "this[\"solargraph.commandPath\"]=\"$solargraphpath\""
 json -I -f "${settings_path}" -e "this[\"solargraph.useBundler\"]=false"
 
-echo '\n=============================================='
-echo '==------------ SETUP COMPLETE!! ------------=='
-echo '==------------ CONGRATULATIONS! ------------=='
-echo '==============================================\n'
-echo '====== Please reload your VSCode Window ======\n'
+printf "\n==============================================================="
+printf "\n==-------------------- SETUP COMPLETE!! ---------------------=="
+printf "\n==-------------------- CONGRATULATIONS! ---------------------=="
+printf "\n==============================================================="
+printf "\n============== Please reload your VSCode Window ===============\n"
